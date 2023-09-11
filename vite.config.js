6316cfa1bb1da4940db6666a26cbe95fe313beb1
Michael Yang <yql.michael@gmail.com>
2023-09-11 14:46:49 +0800

    fix relative path for index page

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   build: {
-    outDir: './docs'
+    outDir: './docs',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
   }
 })
